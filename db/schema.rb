99331086de688647c84cdc970f80d246a84ac1e5
Zuvvo <34513398+Zuvvo@users.noreply.github.com>
2019-02-20 02:02:42 +0100

    fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_09_225011) do
+ActiveRecord::Schema.define(version: 2019_02_20_005731) do
 
-  create_table 'articles', force: :cascade do |t|
-    t.string 'title'
-    t.text 'description'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
